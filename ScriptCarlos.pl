915d5f7ccba31571a80c89a91510b9b5e4cb1bbf
Carlos <carloshenriqueteixeira.18@gmail.com>
2017-04-11 11:45:42 -0300

    Testando funcao

--- ScriptCarlos.pl
+++ ScriptCarlos.pl
@@ -1,14 +1,39 @@
 #!/usr/bin/perl 
 
-print "Digite seu nome\n";
  
 my $nome = "carlos";
-my @dados = _funcaoComDados($nome, $cidade, $idade);
+my @dados = _funcao_com_dados($nome, $cidade, $idade);
 push(@dados);
     
     
-sub _funcaoComDados{
+sub _funcao_com_dados{
 $nome = shift;
 my $cidade = shift;
 my $idade = shift;
+my $carlos;
 }
+	
+
+my $qualificacao = linguagens("Nova","Nova","Antiga");
+ 
+
+sub linguagens{
+
+my $python = shift;
+my $perl = shift;
+my $dot_net = shift; 
+my $msql = shift;
+my $java = shift;
+my $cobol = shift;
+my $php = shift;
+my $javascript = shift;
+
+
+my $funcao = "Sub";
+
+print $funcao."\n"."A Linguagem Python é: ".$python."\n"."A Linguagem Perl é: ".$perl."\n"."A Linguagem DontNet é: ".$dot_net."\n";
+
+}
+
+
+
