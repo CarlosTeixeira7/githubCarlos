9f3bf0b6cdcb0b43cd4d5c3ee82dccab40f79b97
Carlos <carloshenriqueteixeira.18@gmail.com>
2017-04-13 14:33:48 -0300

    adicionando mas subs

--- ScriptCarlos.pl
+++ ScriptCarlos.pl
@@ -42,3 +42,31 @@ my $data_de_criacao = shift;
 }
 
 
+
+
+sub linguagens{
+
+my $python = shift;
+my $perl = shift;
+my $dot_net = shift; 
+my $msql = shift;
+my $java = shift;
+my $cobol = shift;
+my $php = shift;
+my $javascript = shift;
+
+my $funcao = "Sub";
+
+print $funcao."\n"."A Linguagem Python é: ".$python."\n"."A Linguagem Perl é: ".$perl."\n"."A Linguagem DontNet é: ".$dot_net."\n";
+}
+
+
+sub java {
+
+my $nome = shift;
+my $sobrenome = shift;
+my $data_de_criacao = shift;
+}
+
+
+
